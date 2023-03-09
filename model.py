35dc285b117bc0223fac43ec78d4632a40021db3
Ishika Agarwal <105883848+ishikaubc@users.noreply.github.com>
2023-03-08 19:06:59 -0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,23 @@
+#pytorch model and training the dataset
+import torch
+import torch.nn as nn
+
+#feed forward neural net that gets bag of words as input
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size,hidden_size) 
+        self.l2 = nn.Linear(hidden_size,hidden_size) 
+        self.l3 = nn.Linear(hidden_size,num_classes)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x ,y):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        out = self.relu(out)
+        out = self.l3(out)
+        # no activation and no softmax at the end
+        return out
